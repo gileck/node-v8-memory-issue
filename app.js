1c292fcb53ed67ee32ab9c9203e4077cd242202f
Gil Eckstein <gileck@wix.com>
2022-08-30 15:20:48 +0300

    commit

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
+const generateCode = require('./generateCode')
 let max = 0
 const foo = require('./script')
 async function run() {
     // const m = require('/Users/gileck/projects/thunderbolt-simple/thunderbolt/packages/thunderbolt-becky/dist/statics/thunderbolt-platform-simple.bundle.js')
     // const res = await m().execute()
-
-
-    try {
-        foo()
-    } catch (e) {
-
-    }
-
-    // console.log({res})
+    // const foo = require('./script')
+    // require('./script')()
+    foo()
     return {}
 }
 async function start() {
+    generateCode()
     let i = 0
     while (true) {
         console.clear()
         i ++
-        const res = run()
+        run()
         // const rss = res.after.rss
         const rss = process.memoryUsage().rss / 1000 / 1000
         max = rss > max ? rss : max
         console.log(i, Math.round(max), Math.round(rss))
         // console.log(res.after)
         console.log()
-        await new Promise(r => setTimeout(r, 50))
+        await new Promise(r => setTimeout(r, 30))
 
     }
 }
